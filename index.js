aea5885aa503d7d304dc2650f4d1a783ed425361
Ching0810 <neinchin123@gmail.com>
2023-10-14 17:32:43 +0800

    build: add email input validation function

---
+++ index.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('#email-form')
+const input = document.querySelector('#email-input')
+const label = document.querySelector('.error-label')
+const button = document.querySelector('.button')
+const errorMessage = document.querySelector('.error-message')
+
+form.addEventListener('submit', onFormSubmitted) 
+button.addEventListener('click', onFormSubmitted)
+
+function onFormSubmitted(event) {
+  event.preventDefault()
+  event.stopPropagation()
+  const inputValue = input.value.trim()
+  if (!isEmail(inputValue)) {
+    form.classList.add('was-validated')
+    errorMessage.style.display = 'block'
+    label.style.display = 'block'
+  } else {
+    form.classList.remove('was-validated')
+    errorMessage.style.display = 'none'
+    label.style.display = 'none'
+  }
+}
+
+function isEmail (input) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input)
+}
